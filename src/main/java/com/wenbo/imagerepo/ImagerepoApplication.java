f80605ba8fd83ee1a7501a7268199a7d9e2f7218
Wenbo-H <w27han@uwaterloo.ca>
2021-01-15 11:37:17 -0500

    test

--- src/main/java/com/wenbo/imagerepo/ImagerepoApplication.java
+++ src/main/java/com/wenbo/imagerepo/ImagerepoApplication.java
@@ -8,6 +8,7 @@ public class ImagerepoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImagerepoApplication.class, args);
+        System.out.println("hello world");
     }
 
 }
